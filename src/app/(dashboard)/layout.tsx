11f399bd2dec40700b98f84c8a645f95e9a80ed2
Tahara Kazuki <ssp0727.lnc@gmail.com>
2024-09-04 14:28:42 +0900

    update

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -4,9 +4,9 @@ export default function DashboardLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <div>
-      <div>Sidebar</div>
-      <main>{children}</main>
+    <div className="flex h-screen">
+      <div className="hidden bg-red-400 lg:block lg:w-1/4">Sidebar</div>
+      <main className="w-screen bg-blue-400 lg:w-3/4">{children}</main>
     </div>
   );
 }
