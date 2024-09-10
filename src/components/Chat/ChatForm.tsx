d8e1e04303391fda9712edd84fe31fed6f989e71
Tahara Kazuki <ssp0727.lnc@gmail.com>
2024-09-10 13:49:45 +0900

    update

--- src/components/Chat/ChatForm.tsx
+++ src/components/Chat/ChatForm.tsx
@@ -1,13 +1,26 @@
 'use client';
+import { zodResolver } from '@hookform/resolvers/zod';
 import { Send } from 'lucide-react';
 import { useForm } from 'react-hook-form';
+import * as z from 'zod';
 
 import { Button } from '@/components/ui/button';
 import { Form, FormField, FormItem, FormControl } from '@/components/ui/form';
 import { Textarea } from '@/components/ui/textarea';
 
 const ChatForm = () => {
-  const form = useForm();
+  const conversationSchema = z.object({
+    prompt: z.string().min(1, { message: '1文字以上入力してください。' }),
+  });
+
+  type ConversationSchema = z.infer<typeof conversationSchema>;
+
+  const form = useForm<ConversationSchema>({
+    resolver: zodResolver(conversationSchema),
+    defaultValues: {
+      prompt: '',
+    },
+  });
 
   const onSubmit = () => {};
 
