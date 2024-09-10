0dda85f5933fa11caa081afe0bdb51d8d90a83db
Tahara Kazuki <ssp0727.lnc@gmail.com>
2024-09-10 13:25:52 +0900

    update

--- src/components/Chat/ChatForm.tsx
+++ src/components/Chat/ChatForm.tsx
@@ -1,7 +1,26 @@
-import React from 'react';
+import { useForm } from 'react-hook-form';
+
+import { Form, FormField, FormItem, FormControl } from '@/components/ui/form';
+import { Textarea } from '@/components/ui/textarea';
 
 const ChatForm = () => {
-  return <div>ChatForm</div>;
+  const form = useForm();
+
+  return (
+    <Form {...form}>
+      <FormField
+        control={form.control}
+        name="prompt"
+        render={({ field }) => (
+          <FormItem>
+            <FormControl>
+              <Textarea {...field} />
+            </FormControl>
+          </FormItem>
+        )}
+      />
+    </Form>
+  );
 };
 
 export default ChatForm;
